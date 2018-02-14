11581c5641859b74d4280aa4764035c7759c5614
Rytis Alekna <ralekna@exadel.com>
2018-02-14 13:41:41 +0200

    changed imports order

--- src/converter/ConverterView.js
+++ src/converter/ConverterView.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import TextField from 'material-ui/TextField';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
